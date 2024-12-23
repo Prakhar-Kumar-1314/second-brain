d0aa7967c4b5b618c9394faf624168e2b2633f1f
Prakhar-Kumar-1314 <prakharkumar1314@gmail.com>
2024-12-23 23:08:03 +0530

    added the basic project structure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,31 @@
 import express from 'express'
+const app = express();
+const port = 3000;
+
+app.post("/api/v1/signup", (req, res) => {
+
+})
+
+app.post("/api/v1/signin", (req, res) => {
+
+})
+app.post("/api/v1/content", (req, res) => {
+
+})
+app.get("/api/v1/content", (req, res) => {
+
+})
+app.post("/api/v1/share", (req, res) => {
+
+})
+app.get("/api/v1/:shareLink", (req, res) => {
+
+})
+
+app.delete(".api/v1/content", (req, res) => {
+    
+})
+
+app.listen(port, () => {
+    console.log(`Backend works at port ${port}`);
+})
